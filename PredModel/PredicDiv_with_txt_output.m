dbbe930d7c88734267aed4b445bc67f4dfe5574c
Lorette Noiret <noiret.lorette@mg.harvard.edu>
2018-02-08 15:58:14 +0100

    Improved output file, fix issue with feature to inlude

--- PredModel/PredicDiv_with_txt_output.m
+++ PredModel/PredicDiv_with_txt_output.m
@@ -2,119 +2,171 @@
 clear
 close all
 dirdata='/Users/Lorette/Google Drive/YohannsBellaiche/Spatial analysis/data/preprocessed/';
-filedata ='GeneAndPhys_Grid_Steph171222';
+filedata ='GeneAndPhys_Grid_Steph20180201';
 
 load([dirdata,filedata])%,'data*')
-
+clear Folder* dirdata fileout
 
 dirout='/Users/Lorette/Google Drive/YohannsBellaiche/Spatial analysis/results/PredicPhysio/Model/';
 fileout_txt='ExperimentSummmary.txt';
 
 
-Interact01=1;
-list_gene_excl = {'hh','Delta','exd','mirr','sd','wg'};%sd,wg
 
+%{
+gene_excl = {'caup','hth','DIAP1','fz3','mirr','exd','sd','wg'};%sd,wg
+array_list_gene_excl=[];
+for elem=gene_excl
+    array_list_gene_excl = [array_list_gene_excl; union({'hh','Delta'},elem)];
+end
+%}
+%
+% Delta exclut car pas 12apf ; hh exclut car mauvais ; fz3 : exclut non fourni par St?phane
+array_list_gene_excl=[{'hh','Delta','caup','hth','DIAP1','fz3'};{'hh','Delta','mirr','exd','sd','wg'}];
+%}
 cutoff_gene = [0.035	0.02	0.04	0.075	0.027	0.15	0.05	0.04	0.02	0.07	0.025	0.1	0.07	0.004	0.07	0.16	0.03	0.025	0.055	0.06	0.02	0.1	0.05];
-best_lambda  = '1SE';%'MinMSE';'max10';
+%MethGene01=num2str(cutoff_gene);%'ManualImage-Yohanns'%'AutomaticVarianceBased-Stephane'
+list_meth_selec_lambda  = {'1SE';'MaxFeature'}';
+max_feature=10;
 type_model='lasso';
 
 %% Binarize data (rough cutoff selected manually)
-for g=1:length(list_gene)
-    Gene_bin(:,g)=Gene_vect(:,g)>cutoff_gene(g)+0;
-end
 
+%{
+MethGene01={num2str(cutoff_gene);'ManualImage-Yohanns';'AutomaticVarianceBased-Stephane'};
+Gene_bin = MaskSR_vect;
+%}
 
-%% EXCLUDE GENE and Apply mask
-[list_gene,ia]=setdiff(list_gene,list_gene_excl);
-
-
-minAR = min(AreaRatio_vect(:,ia)'); %
-mask = (minAR==1)' .* (Phys_vect(:,3)==1);%~isnan(y);% 1 if area ratio =1 and y has a value
-
-X = Gene_bin(mask==1,ia)+0;
-y = Phys_vect(mask==1,1)+0;
-
-%% Create interaction terms 
-if Interact01==1
-    X=x2fx(X,'interaction');
-    list_gene_interact=x2fx_names(list_gene,'interaction');
-end
+nsimu=0;
+for MethGene01 = {num2str(cutoff_gene),'ManualImage-Yohanns','AutomaticVarianceBased-Stephane'}
     
-%% split sample training/test
-% 85% training-validation / 15% test (n=393/163/97)
-myseed =1;
-rand('seed', myseed);
-cv = cvpartition(length(y),'holdout',0.15);
-
-% Test set
-Xtest = X(test(cv),:);
-ytest = y(test(cv),:);
-
-% Training and validation set
-Xtrain = X(training(cv),:);
-ytrain = y(training(cv),:);
-
-cv = cvpartition(length(ytrain),'holdout',0.25/0.85);
-
-%% Generate models for various prenalty parameter lambda
-
-[B,FitInfo] = lasso(Xtrain,ytrain,'CV',10);%,'PredictorNames',list_gene);
-[ax,figh]=lassoPlot(B,FitInfo,'PlotType','CV');%  green circle ==> |Lambda| with minimum cross-validation error. 
-
-saveas(figh,[dirout,'Lambda_lasso'])
-%}
-%% BEST MODEL - weight, R2, nb of variables selected
-
-
-idx_best_lambda = FitInfo.(['Index',best_lambda]);
-
-% variable selected
-WeightOpti = B(:,idx_best_lambda);% best lambda can be 'MinMSE' or '1SE'
-ListVarOpti= list_gene_interact(WeightOpti~=0);
-TableParaOpti=table(list_gene_interact',WeightOpti,'VariableNames',{'name','weight'});
-TableParaOpti(TableParaOpti.weight==0,:)=[];
-n_var_selected = length(ListVarOpti);
-
-% Predicted values
-ytrain_pred = Xtrain*B(:,idx_best_lambda)+FitInfo.Intercept(idx_best_lambda);
-ytest_pred = Xtest*B(:,idx_best_lambda)+FitInfo.Intercept(idx_best_lambda);
-
-% R2 and R2adj
-[R2train,R2adjtrain] = myR2(ytrain,ytrain_pred,n_var_selected);%train set
-[R2test,R2adjtest] = myR2(ytest,ytest_pred,n_var_selected);%test set
-
-median_err_train = nanmedian( ytrain- ytrain_pred );
-median_err_test  = nanmedian( ytest - ytest_pred  )  ;
-
-%stat_err_train = [ min(abs(ytrain-ytrain_pred)),nanmedian((ytrain-ytrain_pred)),max(abs(ytrain-ytrain_pred))];
-%stat_err_test  = [ min(abs(ytest-ytest_pred))  ,nanmedian((ytest-ytest_pred))  ,max(abs(ytest-ytest_pred))];
-pct_err_train=( ytrain(:)-  ytrain_pred(:))./ytrain(:);
-pct_err_test=( ytest(:)-  ytest_pred(:))./ytest(:);
-pct_err_train_within10=length(find(pct_err_train>=-0.10&pct_err_train<=0.10))/length(pct_err_train);
-pct_err_train_within20=length(find(pct_err_train>=-0.20&pct_err_train<=0.20))/length(pct_err_train);
-pct_err_test_within10=length(find(pct_err_test>=-0.10&pct_err_test<=0.10))/length(pct_err_test);
-pct_err_test_within20=length(find(pct_err_test>=-0.20&pct_err_test<=0.20))/length(pct_err_test);
-
-
-%
-h=figure
-scatter(ytest,ytest_pred);
-hold on
-plot([0,max(ytest)],[0,max(ytest)])
-hold off
-title(['Test set: observed vs predicted (lambda =',best_lambda,')'])
-saveas(h,[dirout,'YvsYhat_test_set_MSE'])
-%
-WriteResultModel(dirdata,dirout,fileout_txt,list_gene,list_gene_excl,Interact01,...
- cutoff_gene,myseed, type_model,FitInfo.Lambda(idx_best_lambda),FitInfo.MSE(idx_best_lambda),mat2str([length(ytrain),length(ytest)]),...
- n_var_selected,TableParaOpti,R2train,R2adjtrain,pct_err_train_within10,pct_err_train_within20,R2test,R2adjtest,pct_err_test_within10,pct_err_test_within20);
-%}
-clear h
-
-
-
+    
+    switch MethGene01{:}
+        case 'AutomaticVarianceBased-Stephane'
+            Gene_bin = MaskSR_vect;
+        case 'ManualImage-Yohanns'
+            Gene_bin = MaskYB_vect;
+        otherwise
+            for g=1:length(list_gene)
+                Gene_bin(:,g)=Gene_vect(:,g)>cutoff_gene(g)+0;
+            end
+    end
+    
+    %% ------------------------------------------------------------------------
+    %% %%%%%%%%%%%%%%%%%%%
+    
+    for Interact01=0:1
+        
+        [nl,~] = size(array_list_gene_excl);
+        for ix = 1:nl
+            list_gene_excl={array_list_gene_excl{ix,:}};
+            
+            %% EXCLUDE GENE and Apply mask
+            [list_gene_incl,ia]=setdiff(list_gene,list_gene_excl);
+            
+            % Most restrictive mask : only keep point if area ratio 1
+            minAR = min(AreaRatio_vect(:,ia),[],2); % min per row
+            mask = (minAR==1) .* (Phys_vect(:,3)==1);% 1 if area ratio =1 and y has a value
+            
+            X = Gene_bin(mask==1,ia)+0;
+            y = Phys_vect(mask==1,1)+0;
+            
+            %% Create interaction terms
+            if Interact01==1
+                X=x2fx(X,'interaction');
+                list_gene_interact=x2fx_names(list_gene_incl,'interaction');
+                % remove column corresponding to constant
+                X=X(:,2:end);
+                list_gene_interact=list_gene_interact(2:end);
+            else
+                list_gene_interact=list_gene_incl;
+            end
+            
+            %% split sample training/test
+            % 85% training-validation / 15% test (n=393/163/97)
+            myseed =1;
+            rand('seed', myseed);
+            cv = cvpartition(length(y),'holdout',0.15);
+            
+            % Test set
+            Xtest = X(test(cv),:);
+            ytest = y(test(cv),:);
+            
+            % Training and validation set
+            Xtrain = X(training(cv),:);
+            ytrain = y(training(cv),:);
+            
+            %  cv = cvpartition(length(ytrain),'holdout',0.25/0.85);
+            
+            %% Generate models for various prenalty parameter lambda
+            rand('seed', 19479);
+            [B,FitInfo] = lasso(Xtrain,ytrain,'CV',10);%,'PredictorNames',list_gene_incl);
+            %[ax,figh]=lassoPlot(B,FitInfo,'PlotType','CV');%  green circle ==> |Lambda| with minimum cross-validation error.
+            % saveas(figh,[dirout,'Lambda_lasso_Interact',MethGene01,num2str(Interact01),'_',num2str(nsimu)])
+            % nsimu=nsimu+1;
+            %}
+            %% BEST MODEL - weight, R2, nb of variables selected
+            for meth =list_meth_selec_lambda
+                
+                if ismember(meth,'MaxFeature')
+                    n_feature_per_model =sum(B~=0);
+                    idx_max_feature = find(n_feature_per_model<=max_feature);
+                    [~,temp]=min(FitInfo.MSE(:,idx_max_feature));
+                    idx_best_lambda = idx_max_feature(temp);
+                    
+                else
+                    idx_best_lambda = FitInfo.(['Index',meth{:}]);
+                end
+                
+                
+                
+                % variable selected
+                WeightOpti = B(:,idx_best_lambda);% best lambda can be 'MinMSE' or '1SE'
+                ListVarOpti= list_gene_interact(WeightOpti~=0);
+                TableParaOpti=table(list_gene_interact',WeightOpti,'VariableNames',{'name','weight'});
+                TableParaOpti(TableParaOpti.weight==0,:)=[];
+                n_var_selected = length(ListVarOpti);
+                
+                % Predicted values
+                ytrain_pred = Xtrain*B(:,idx_best_lambda)+FitInfo.Intercept(idx_best_lambda);
+                ytest_pred = Xtest*B(:,idx_best_lambda)+FitInfo.Intercept(idx_best_lambda);
+                
+                % R2 and R2adj
+                [R2train,R2adjtrain] = myR2(ytrain,ytrain_pred,n_var_selected);%train set
+                [R2test,R2adjtest] = myR2(ytest,ytest_pred,n_var_selected);%test set
+                
+                median_err_train = nanmedian( ytrain- ytrain_pred );
+                median_err_test  = nanmedian( ytest - ytest_pred  )  ;
+                
+                %stat_err_train = [ min(abs(ytrain-ytrain_pred)),nanmedian((ytrain-ytrain_pred)),max(abs(ytrain-ytrain_pred))];
+                %stat_err_test  = [ min(abs(ytest-ytest_pred))  ,nanmedian((ytest-ytest_pred))  ,max(abs(ytest-ytest_pred))];
+                pct_err_train          =( ytrain(:)-  ytrain_pred(:))./ytrain(:);
+                pct_err_test           =( ytest(:)-  ytest_pred(:))./ytest(:);
+                pct_err_train_within10 =length(find(pct_err_train>=-0.10&pct_err_train<=0.10))/length(pct_err_train);
+                pct_err_train_within20 =length(find(pct_err_train>=-0.20&pct_err_train<=0.20))/length(pct_err_train);
+                pct_err_test_within10  =length(find(pct_err_test>=-0.10&pct_err_test<=0.10))/length(pct_err_test);
+                pct_err_test_within20  =length(find(pct_err_test>=-0.20&pct_err_test<=0.20))/length(pct_err_test);
+                
+                
+                %{
+    h=figure
+    scatter(ytest,ytest_pred);
+    hold on
+    plot([0,max(ytest)],[0,max(ytest)])
+    hold off
+    title(['Test set: observed vs predicted (lambda =',meth{:},')'])
+    saveas(h,[dirout,'YvsYhat_test_set_MSE'])
+                %}
+                WriteResultModel(filedata,dirout,fileout_txt,list_gene_interact,list_gene_excl,Interact01,...
+                    MethGene01{:},myseed, type_model,meth{:},FitInfo.Lambda(idx_best_lambda),FitInfo.MSE(idx_best_lambda),mat2str([length(ytrain),length(ytest)]),...
+                    n_var_selected,TableParaOpti,FitInfo.Intercept(idx_best_lambda),R2train,R2adjtrain,pct_err_train_within10,pct_err_train_within20,R2test,R2adjtest,pct_err_test_within10,pct_err_test_within20);
+                %}
+                clear h
+            end % type of lambda
+        end %for ix:list gene
+    end % for jx: interaction
+end %mask YB SR LN
 %% FULL SAMPLE: look for best model (find colorbar)
-% comparison original versus predicted 
+% comparison original versus predicted
 %{
 % pred all point
 y_pred = NaN(length(Phys_vect),1);
@@ -123,7 +175,7 @@ y_pred(mask==1) = X*B(:,idx_best_lambda)+FitInfo.Intercept(idx_best_lambda);
 cmin =min(min([y_pred, y_pred,Phys_vect(:,1)]));
 cmax =max(max([y_pred, y_pred,Phys_vect(:,1)]));
 
-% reshape for image    
+% reshape for image
 y_pred = reshape(y_pred,size(data_phys.div));%training set
 %}
 %% FULL SAMPLE (train + test sets): graphical inspection
@@ -135,7 +187,7 @@ set(gca,'xtick',[]);set(gca,'ytick',[])
 title('Division (observed)')
 
 subplot(2,2,2)% predicted image
-imagesc(y_pred,[cmin,cmax]); 
+imagesc(y_pred,[cmin,cmax]);
 set(gca,'xtick',[]);set(gca,'ytick',[])
 title('Division (predicted)')
 
@@ -166,9 +218,9 @@ end
 %% file exchange
 function res=x2fx_names(PredictorNames,model)
 %function res = x2fx_name(PredictorNames, model)
-%   returns a cell array of "compound" predictor variable names that 
-%   correpond to the x2fx'ed matrix of "normal" predictor variable names.  
-%  
+%   returns a cell array of "compound" predictor variable names that
+%   correpond to the x2fx'ed matrix of "normal" predictor variable names.
+%
 
 %set up a table of primes (don't feel like calling prime() every time)
 prime_table = [...
@@ -177,7 +229,7 @@ prime_table = [...
 
 %check if have enough primes
 %can probably do that n/ln(n) thing to figure out how many primes we actually need, but 50 should be enough for anybody (just like 640KB of memory)
-if length(prime_table)<length(PredictorNames)  
+if length(prime_table)<length(PredictorNames)
     error('internal error: increase size of prime table (really mixing more than 50 variables???)')
 end
 
@@ -213,4 +265,3 @@ for i = 1:length(mix)
     end
 end
 end
- 
--- PredModel/WriteResultModel.m
+++ PredModel/WriteResultModel.m
@@ -1,6 +1,6 @@
 function WriteResultModel(dirdata,dirout,fileout_txt,list_gene,list_gene_excl,Interact01,...
- cutoff_gene,myseed, type_model,best_lambda,MSE,size_sets,...
- n_var_selected,TableParaOpti,R2train,R2adjtrain,pct_err_train_within10,pct_err_train_within20,R2test,R2adjtest,pct_err_test_within10,pct_err_test_within20);
+ MethGene01,myseed, type_model,meth,best_lambda,MSE,size_sets,...
+ n_var_selected,TableParaOpti,Weight_Intercept,R2train,R2adjtrain,pct_err_train_within10,pct_err_train_within20,R2test,R2adjtest,pct_err_test_within10,pct_err_test_within20)
 
 
 
@@ -18,8 +18,7 @@ if exist([fileout],'file')==2
     
 else % if file does not exist, add header
     fileID = fopen(fileout,'w');
-    fprintf(fileID,'DataPath \t Var_Incl \t Var_Excl \t Interact01 \t cutoff \t seed \t Model \t lambda \t size(train,test) \tn_var_select \t Var_select \t Weight \t MSE_train \t R2_train \t R2adj_train \t pct_err_train_within10 \t pct_err_train_within20  \t R2_test \t R2adj_test \t pct_err_test_within10 \t pct_err_test_within20 \n') ;   
-    %fprintf(fileID,'DataPath;Var_Incl.;Var_Excl.;Interaction01;threshold;seed;Model;lambda;Model_description;MSE;SizeTest;Weight\n') ;   
+    fprintf(fileID,'Date \t DataPath \t Var_Incl \t Var_Excl \t Interact01 \t MethToBinarize \t seed \t Model \t TypeLambda \t lambda \t size(train,test) \tn_var_select \t Var_select \t Weight \t Intercept \t MSE_train \t R2_train \t R2adj_train \t pct_err_train_within10 \t pct_err_train_within20  \t R2_test \t R2adj_test \t pct_err_test_within10 \t pct_err_test_within20 \n') ;   
 end
 
 % write relevant output
@@ -29,18 +28,23 @@ end
 
 temp = list_gene';
 %temps = temp{:};
+fprintf(fileID,'%s\t',datetime('now'));
 fprintf(fileID,'%s\t',dirdata);
 fprintf(fileID,'%s,' ,list_gene{:})     ;fprintf(fileID,'\t');
 fprintf(fileID,'%s,' ,list_gene_excl{:});fprintf(fileID,'\t');
 fprintf(fileID,'%d\t',Interact01); 
-fprintf(fileID,'%.4f,' ,cutoff_gene);   fprintf(fileID,'\t')
+fprintf(fileID,'%s\t' ,MethGene01);
 fprintf(fileID,'%f\t',myseed); 
 fprintf(fileID,'%s\t',type_model); 
+fprintf(fileID,'%s\t',meth); 
 fprintf(fileID,'%s\t',best_lambda); 
 fprintf(fileID,'%s\t',size_sets); 
 fprintf(fileID,'%d\t',n_var_selected);
 fprintf(fileID,'%s,' ,TableParaOpti.name{:});    fprintf(fileID,'\t');
 fprintf(fileID,'%f,' ,TableParaOpti.weight);    fprintf(fileID,'\t');
+fprintf(fileID,'%s\t',Weight_Intercept); 
+
+
 fprintf(fileID,'%s\t',MSE); 
 
 fprintf(fileID,'%.3f\t',R2train);
